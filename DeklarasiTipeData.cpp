5b9d3f27971fa32444cfe64431dd1724fbaf5efd
rahmadi <rahmadiempatenam@gmail.com>
2023-11-05 23:36:52 +0800

    Hari 28

---
+++ DeklarasiTipeData.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //deklarasi variabel
+    int i;
+    double d;
+    char c;
+    char*s;
+    
+    // mengisi nilai kedalam variabel
+    i = 88;
+    d = 4.675;
+    c = 'C';
+    s = (char*)"Contoh string";
+
+    // menampilkan nilai variabel
+    cout<<"Nilai i : "<<i<<endl;
+    cout<<"Nilai d : "<<d<<endl;
+    cout<<"Nilai c : "<<c<<endl;
+    cout<<"Nilai s : "<<s<<endl;
+    return 0;
+}
