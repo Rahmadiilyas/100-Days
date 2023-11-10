4a6b6e48ef68240afbf541a8ac5296680904126c
Rahmadi <147220595+Rahmadiilyas@users.noreply.github.com>
2023-11-10 21:47:01 +0700

    Hari 33
    Hari 33

--- Main.java
+++ Main.java
@@ -1,28 +1,20 @@
 package day100;
-
+import java.util.Scanner;
 public class Main {
     
     public static void main(String[] args) {
         
-        String nama_mahasiswa = "Amrillah";
-        int harga_komputer = 55000000;
-        int gaji_pokok = 5000000;
-        int uang_kost = 750000;
-        int tabungan_perbulan = 1500000;
-        int lama_waktu = 0;
-        int target_bulan = 12;
+        Scanner input = new Scanner(System.in);
         
-        while (harga_komputer > 0 && lama_waktu < target_bulan){
-            harga_komputer -= tabungan_perbulan;
-            lama_waktu++;
+        System.out.println("Tabel Perkalian");
+        
+        System.out.print("Masukkan angka : ");
+        int angka = input.nextInt();
+        
+        System.out.println("Berikut adalah tabel perkalian " +angka);
+        
+        for(int i=0; i<10; i++){
+            System.out.println(angka +" x "+ (i+1) +" = "+ angka*(i+1));
         }
-        
-        System.out.println("Nama mahasiswa: "+nama_mahasiswa);
-        System.out.println("Harga komputer sultan: "+harga_komputer);
-        System.out.println("Gaji pokok perbulan: "+gaji_pokok);
-        System.out.println("Uang kost perbulan: "+uang_kost);
-        System.out.println("Uang tabungan perbulan: "+tabungan_perbulan);
-        System.out.println("Lama waktu untuk membeli komputer sultan: "+(lama_waktu)+" bulan");
     }
 }
-    
