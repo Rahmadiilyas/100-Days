f18b641e355efa85527d2d09db9c8e885a712d4f
Rahmadi <147220595+Rahmadiilyas@users.noreply.github.com>
2023-11-11 19:56:56 +0700

    Hari 34
    Hari 34

---
+++ BelajarArray.java
@@ -0,0 +1,14 @@
+package day.pkg100;
+
+public class BelajarArray {
+    public static void main(String[] args) {
+        int [] a = {4,7,9,10,11};
+        int jml = a.length;
+        
+        for (int i = 0; i<jml; i++){
+            System.out.print(a[i]+ " ");
+        }
+        System.out.println("");
+    }
+  
+}
