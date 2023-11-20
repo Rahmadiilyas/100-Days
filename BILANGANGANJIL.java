8919cad0f520935e590c486b2ffb82d8096adc08
rahmadi <rahmadiempatenam@gmail.com>
2023-11-20 22:16:51 +0800

    Hari 43

---
+++ BILANGANGANJIL.java
@@ -0,0 +1,10 @@
+
+package day.pkg100;
+
+public class BILANGANGANJIL {
+    public static void main(String[] args) {
+        for(int i = 1; i<99; i+=2){
+            System.out.println(i);
+        }
+    }
+}
