edcbcd6a7824c76fdda65355a77d1bf7c9834b16
rahmadi <rahmadiempatenam@gmail.com>
2023-11-25 22:27:15 +0800

    Hari 48

---
+++ arrayHewan.java
@@ -0,0 +1,17 @@
+
+package day.pkg100;
+
+public class arrayHewan {
+    public static void main(String[] args) {
+        
+    
+        String[] namaHewan = {
+            "Singa", "Gajah", "Jerapah", "Harimau", "Kucing", "Anjing", "Kanguru", "Kuda", "Zebra", "Beruang"
+        };
+
+        // Menampilkan semua nilai dari indeks array namaHewan
+        for (String hewan : namaHewan) {
+            System.out.println(hewan);
+        }
+    }
+}
