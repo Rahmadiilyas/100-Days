7b5fa9d972312acff580ad2adf386eb666397a2f
rahmadi <rahmadiempatenam@gmail.com>
2023-12-04 23:35:42 +0800

    Hari 57

---
+++ POLAPERSEGI.java
@@ -0,0 +1,12 @@
+
+package day.pkg100;
+public class POLAPERSEGI {
+    public static void main(String[] args) {
+        for(int i=0;i<4;++i){
+            for(int j=0;j<4;j++){
+                System.out.print("#");
+            }
+            System.out.println();
+    }
+    }
+}
