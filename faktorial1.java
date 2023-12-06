e5da1f321bfa8af6d930987b32a3bfa3ffbe2892
rahmadi <rahmadiempatenam@gmail.com>
2023-12-06 21:45:25 +0800

    Hari 59

---
+++ faktorial1.java
@@ -0,0 +1,25 @@
+
+package day.pkg100;
+import java.util.Scanner;
+public class faktorial1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int angka ;
+        System.out.print("Masukkan angka : ");
+        angka = input.nextInt();
+        int hasil = angka;
+        System.out.print(angka+"!=");
+        for (int i = angka; i>0; i--){
+            System.out.print(i);
+            if (i>1){
+                System.out.print(" x ");
+            }
+            if (angka != i){
+                hasil *=i;
+            }
+            }
+        System.out.println("");
+        System.out.println("hasil = "+hasil);
+        }
+    }
+
