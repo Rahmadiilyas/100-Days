5248e21483b290b9577face8fec4e16b8eb4ab77
rahmadi <rahmadiempatenam@gmail.com>
2023-12-08 21:29:01 +0800

    Hari 61

---
+++ jajargenjang.java
@@ -0,0 +1,21 @@
+
+package day.pkg100;
+import java.util.Scanner;
+public class jajargenjang {
+    public static void main(String[] args) {
+   
+        System.out.println("======Menghitung luas Jajar Genjang=======");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Alas : ");
+        int alas = input.nextInt();
+        System.out.print("Masukkan tinggi : ");
+        int tinggi = input.nextInt();
+        
+        int luas = alas*tinggi;
+        
+        System.out.println("Luas Jajar genjang : " + luas);
+    }
+    
+}
+
+
