5dcb6b7a8c0a45168670d7bb499ac52a89b44604
rahmadi <rahmadiempatenam@gmail.com>
2023-12-09 23:00:34 +0800

    Hari 62

---
+++ ArrayMenampungHewan.java
@@ -0,0 +1,13 @@
+
+package day.pkg100;
+
+public class ArrayMenampungHewan {
+    public static void main(String[] args) {
+        String [] namahewan = {"anjing,kucing,kelinci,kuda,ayam"};
+        
+        System.out.println("Nama nama Hewan");
+        for (String hewan : namahewan){
+            System.out.println(namahewan);
+        }
+    }
+}
