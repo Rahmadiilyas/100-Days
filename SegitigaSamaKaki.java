31a91c932d27c91d6274afbc570bbded72f4da19
rahmadi <rahmadiempatenam@gmail.com>
2023-12-11 22:40:59 +0800

    Hari 64

---
+++ SegitigaSamaKaki.java
@@ -0,0 +1,23 @@
+
+package day.pkg100;
+import java.util.Scanner;
+public class SegitigaSamaKaki {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan tinggi segitiga: ");
+        int tinggi = input.nextInt();
+
+        System.out.println("Segitiga Sama Kaki:");
+        for (int i = 1; i <= tinggi; i++) {
+            for (int j = 1; j <= tinggi - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+ 
