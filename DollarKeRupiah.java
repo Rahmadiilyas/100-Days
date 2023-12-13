cdcce435a5aaefa0a8c717c007ad775411203510
rahmadi <rahmadiempatenam@gmail.com>
2023-12-13 19:21:01 +0800

    Hari 66

---
+++ DollarKeRupiah.java
@@ -0,0 +1,19 @@
+
+package day.pkg100;
+import java.util.Scanner;
+public class DollarKeRupiah {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+        
+        System.out.println("Konversi mata uang dollar ke rupiah");
+        System.out.println("");
+      
+        System.out.print("Masukkan jumlah uang dalam bentuk dollar: $");
+        int dollar = input.nextInt();
+      
+        int rupiah = dollar * 15000;
+      
+        System.out.println("$"+dollar+" jika dikonversi ke rupiah adalah Rp."+rupiah);
+    }
+}
+ 
