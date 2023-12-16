d5678bfa3c2dda95b8e3748fdb40ffcc2f0f6db0
rahmadi <rahmadiempatenam@gmail.com>
2023-12-16 21:18:17 +0800

    Hari 69

---
+++ Volumebalok.java
@@ -0,0 +1,25 @@
+
+package day.pkg100;
+import java.util.Scanner;
+public class Volumebalok {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.println("Selamat Datang di Program Hitung Volume Balok");
+        
+        System.out.print("Masukkan panjang balok: ");
+        int panjang = input.nextInt();
+        
+        System.out.print("Masukkan lebar balok: ");
+        int lebar = input.nextInt();
+        
+        System.out.print("Masukkan tinggi balok: ");
+        int tinggi = input.nextInt();
+        
+        int volume = panjang * lebar * tinggi;
+        
+        System.out.println("");
+        System.out.println("Volume dari balok tersebut adalah "+volume);
+    }
+}
+ 
