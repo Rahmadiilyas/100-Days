26ec5855ab8e60d4d7d0bae15866a40391f3fbdc
rahmadi <rahmadiempatenam@gmail.com>
2023-12-18 21:39:26 +0800

    Hari 71

---
+++ prosedurfungsi.java
@@ -0,0 +1,20 @@
+package day.pkg100;
+public class prosedurfungsi {
+    public static void mahasiswa(){
+        String nama = "Rahmadi";
+        int umur = 18;
+        System.out.println(nama+"- "+umur+" th");
+    
+}
+    public static String mahasiswi(){
+        String nama = "Intan asyura";
+        int umur = 19;
+        return nama+"- "+umur+" th";
+    }
+    public static void main(String[] args) {
+        mahasiswa();
+        System.out.println(mahasiswi());
+    }
+    
+}
+
