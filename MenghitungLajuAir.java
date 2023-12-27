01251e9f298057bf3cebea24c23c8f5bb2124c56
rahmadi <rahmadiempatenam@gmail.com>
2023-12-27 22:11:54 +0800

    Hari 80

---
+++ MenghitungLajuAir.java
@@ -0,0 +1,19 @@
+
+package day.pkg100;
+import java.util.Scanner;
+public class MenghitungLajuAir {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+
+        System.out.print("Masukkan volume air (dalam liter): ");
+        double volume_air = input.nextDouble();
+
+        System.out.print("Masukkan waktu (dalam detik): ");
+        double waktu = input.nextDouble();
+
+        double laju_air = volume_air / waktu;
+
+        System.out.println("Laju air adalah: "+laju_air+" liter/detik");
+    }
+}
+
