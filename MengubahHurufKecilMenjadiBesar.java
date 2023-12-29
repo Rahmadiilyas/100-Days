471043869decc5edf534edd9025bd38732c7f5dd
rahmadi <rahmadiempatenam@gmail.com>
2023-12-29 22:09:05 +0800

    Hari 82

---
+++ MengubahHurufKecilMenjadiBesar.java
@@ -0,0 +1,14 @@
+
+package day.pkg100;
+import java.util.Scanner;
+public class MengubahHurufKecilMenjadiBesar {
+    public static void main(String[] args) {
+        Scanner up = new Scanner(System.in);
+        System.out.print("Masukan kata : ");
+        String kata = up.nextLine();
+        System.out.println("________________________");
+        System.out.println("kata awal       : "+kata);
+        System.out.println("hasil konversi  : "+kata.toUpperCase()+"\nARIGATO : )");
+    }
+}
+ 
